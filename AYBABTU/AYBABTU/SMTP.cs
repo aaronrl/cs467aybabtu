b18e74566c1ae859d334e7419516cd7acc68e321
aaron.r.lindstrom <aaron.r.lindstrom@611ef15e-89d6-11dd-b20b-83d96e936561>
2008-09-29 21:42:10 +0000

    created smtp class for sending messages

---
+++ AYBABTU/AYBABTU/SMTP.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mail;
+namespace AYBABTU
+{
+    class SMTP
+    {
+        public static bool sendMessage(Message msg)
+        {
+            
+        }
+
+    }
+}
