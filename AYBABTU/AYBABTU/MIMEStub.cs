95c4e588b2d5a7e0a60985913a43ef95c47d676b
aaron.r.lindstrom <aaron.r.lindstrom@611ef15e-89d6-11dd-b20b-83d96e936561>
2008-12-02 00:14:31 +0000

    reverting mimestub

--- AYBABTU/AYBABTU/MIMEStub.cs
+++ AYBABTU/AYBABTU/MIMEStub.cs
@@ -13,50 +13,10 @@ namespace AYBABTU
 
         public static Message[] returnMessages(string[] incomingMessages)
         {
-<<<<<<< .mine
-            Message[] messages = new Message[incomingMessages.Length()];
-            String[] MessageContents;
-            Message tempMessage;
-            int count;
-=======
             Message[] messages = new Message[incomingMessages.Length];
->>>>>>> .r88
 
-<<<<<<< .mine
-            // Going through each message
-            for (int i = 0; i < incomingMessages.Length(); i++)
-=======
             for (int i = 0; i < incomingMessages.Length; i++)
->>>>>>> .r88
             {
-<<<<<<< .mine
-                MessageContents = incomingMessages[i].Split('\n');
-
-                //get to, from, subject, date, and cc fields
-                for (int j = 0; j < MessageContents.Length(); j++)
-                {
-                    if(MessageContents[j].Substring(0,3).Equals("To:"))
-                    {
-                        
-                    }
-                    if (MessageContents[j].Substring(0, 3).Equals("CC:"))
-                    {
-
-                    }
-                    if (MessageContents[j].Substring(0, 5).Equals("From:"))
-                    {
-
-                    }
-                    if (MessageContents[j].Substring(0, 5).Equals("Date:"))
-                    {
-
-                    }
-                    if (MessageContents[j].Substring(0, 8).Equals("Subject:"))
-                    {
-
-                    }
-                }
-=======
                 //messages[i] = new Message("Aaron@mail.com", "Test@mail.com", "This is a test message: " + i, "Hello! \nI am a test message for this program! \nI hope you like me!");
                 messages[i] = new Message();
                 messages[i].RawMessage = incomingMessages[i];
@@ -64,7 +24,6 @@ namespace AYBABTU
                 messages[i].Subject = "Test " + i;
                 messages[i].From = "test" + i + "@test.com";
                 messages[i].Date = "11/11/1111";
->>>>>>> .r88
             }
 
             return messages;
