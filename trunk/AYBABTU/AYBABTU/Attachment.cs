a85a6699613bd486c8fc090b97f5f23407caea6e
aaron.r.lindstrom <aaron.r.lindstrom@611ef15e-89d6-11dd-b20b-83d96e936561>
2008-12-04 14:24:39 +0000

    Created attachment class that handles decoding

---
+++ trunk/AYBABTU/AYBABTU/Attachment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+//using System.Windows.Forms;
+
+namespace AYBABTU
+{
+    class Attachment
+    {
+        private string encodedFile = "";
+        private string fileName = "";
+        public byte[] decodedFile;
+
+        public Attachment(string encoding, string name)
+        {
+            encodedFile = encoding;
+            encodedFile = encodedFile.Replace("\n", "");
+            fileName = name;
+            decodedFile = Convert.FromBase64String(encodedFile);
+            //MessageBox.Show(Convert.ToString(encodedFile.Length % 4));
+        }
+
+        public void writeFileToSystem(string path)
+        {
+            StreamWriter sw = new StreamWriter(path);
+            Stream bs = sw.BaseStream;
+
+            byte[] b = decodedFile;
+
+            bs.Write(b, 0, b.Length);
+
+            sw.Close();
+            sw = null;
+        }
+    }
+}
+
